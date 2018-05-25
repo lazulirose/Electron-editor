e8ce94ecc3e6c92c5a54cff3c725a944753386a6
Ancy <lazuliroseancy@gmail.com>
2018-05-25 22:35:52 +0900

    Add test

--- src/renderer/components/Previewer.jsx
+++ src/renderer/components/Previewer.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import marked from "marked";
 import style from "./Previewer.css";
+import emojione from "emojione";
+
+const renderer = new marked.Renderer();
+renderer.text = (text) => {
+  return emojione.shortnameToImage(text);
+};
 
 export default function Previewer(props) {
   return (
@@ -9,7 +15,7 @@ export default function Previewer(props) {
       className={`${props.className} ${style.previewer}`}
     >
       <span
-        dangerouslySetInnerHTML={{ __html: marked(props.value) }}
+        dangerouslySetInnerHTML={{ __html: marked(props.value, { renderer }) }}
       />
     </div>
   );
